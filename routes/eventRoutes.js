4c233735cbe5ac24d33ff7529299d46673cefa8b
Omar Banat <o.j.banat@gmail.com>
2023-11-15 11:34:42 +0200

    Events

---
+++ routes/eventRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  getAll,
+  getByID,
+  addEvent,
+  updateByID,
+  deleteByID,
+} = require('../controllers/eventController');
+
+router.get('/getAll', getAll);
+router.get('/getByID/:ID', getByID);
+router.post('/add', addEvent);
+router.put('/update/:ID', updateByID);
+router.delete('/delete/:ID', deleteByID);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const venueRoutes = require('./routes/venueRoutes');
+const eventRoutes = require('./routes/eventRoutes');
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/user', userRoutes);
 app.use('/venue', venueRoutes);
+app.use('/event', eventRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
